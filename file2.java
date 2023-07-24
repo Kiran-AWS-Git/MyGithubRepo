57f229709232b70b266e0160222d4688b4699248
Kiran-GitLocal <kiran.musumalli@gmail.com>
2023-07-24 07:59:22 +0530

    Committing file2 and file4 to Git Lical Repository

---
+++ file2.java
@@ -0,0 +1,10 @@
+import java.io.*;
+public class Sample2
+{
+	public static void main(String args[])
+	{
+		System.out.println("This is the second sample java code");
+		System.out.println("Pushing second batch of files to GitHub");
+	}
+}
+
